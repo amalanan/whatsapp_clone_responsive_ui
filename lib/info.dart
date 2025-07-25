067e064ee74ba36c031029cb1bd230cbda711fbd
Amal Anan <133678416+amalanan@users.noreply.github.com>
2025-07-25 21:24:43 +0300

    Create info.dart

---
+++ lib/info.dart
@@ -0,0 +1,152 @@
+const info = [
+  {
+    'name': 'Me',
+    'message': 'this is me trying',
+    'time': '3:53 pm',
+    'profilePic': 'assets/images/12.png'
+  },
+  {
+    'name': 'Temi',
+    'message': 'Missing You',
+    'time': '2:25 pm',
+    'profilePic': 'assets/images/14.png'
+  },
+  {
+    'name': 'Soad Sister',
+    'message': 'Hello, whats up',
+    'time': '2:25 pm',
+    'profilePic': 'assets/images/2.png'
+  },
+  {
+    'name': 'Asmaa Sister',
+    'message': 'Hello, I want to sleep.',
+    'time': '1:03 pm',
+    'profilePic': 'assets/images/3.png'
+  },
+  {
+    'name': 'Dad',
+    'message': 'Call me, have some work',
+    'time': '12:06 pm',
+    'profilePic': 'assets/images/4.png'
+  },
+  {
+    'name': 'Mom',
+    'message': 'You ate food?',
+    'time': '10:00 am',
+    'profilePic':
+    'assets/images/5.png'
+  },
+  {
+    'name': 'Aseel Friend',
+    'message': 'Yo!!!!! Long time, no see!?',
+    'time': '9:53 am',
+    'profilePic': 'assets/images/6.png'
+  },
+  {
+    'name': 'Nada Friend',
+    'message': 'Am I fat?',
+    'time': '7:25 am',
+    'profilePic': 'assets/images/7.png'
+  },
+  {
+    'name': 'Heba Sister',
+    'message': 'I am from International Olym...',
+    'time': '6:02 am',
+    'profilePic': 'assets/images/8.png'
+  },
+  {
+    'name': 'Farouq Brother',
+    'message': 'Lets Code!',
+    'time': '4:56 am',
+    'profilePic': 'assets/images/9.png'
+  },
+  {
+    'name': 'Nesma Friend',
+    'message': 'Images by Unsplash',
+    'time': '1:00 am',
+    'profilePic':
+    'assets/images/10.png'
+  },
+  {
+    'name': 'Mahmoud (";',
+    'message': 'Are you interested in AOT?',
+    'time': '2:25 pm',
+    'profilePic': 'assets/images/13.png'
+  },
+];
+
+const messages = [
+  {"isMe": false, "text": "Hey What is up with you!!", "time": "10:00 am"},
+  {"isMe": true, "text": "im fine,wbu?", "time": "11:00 am"},
+  {"isMe": false, "text": "I am great man!", "time": "11:01 am"},
+  {
+    "isMe": false,
+    "text": "Just messaged cuz I had some work.",
+    "time": "11:01 am"
+  },
+  {"isMe": true, "text": "Obviously, say", "time": "11:03 am"},
+  {
+    "isMe": false,
+    "text": "haha I wanted you to check out the last Episode of AOT ^^",
+    "time": "11:04 am"
+  },
+  {
+    "isMe": true,
+    "text": " Oh, is it coming to an End!",
+    "time": "11:05 am"
+  },
+  {
+    "isMe": false,
+    "text": "Yes! I'm very Sad about it",
+    "time": "11:06 am",
+  },
+  {
+    "isMe": true,
+    "text": "Let's Watch it",
+    "time": "11:15 am",
+  },
+
+
+  {"isMe": false, "text": "Let's Go!",
+    "time": "11:17 am"},
+  {
+    "isMe": false,
+    "text": "I'm so Excited",
+    "time": "11:16 am"
+  },
+  {
+    "isMe": true,
+    "text": "Yes, Me Too!",
+    "time": "11:17 am"
+  },
+  {
+    "isMe": false,
+    "text": "Cool, did you get your Snacks?",
+    "time": "11:18 am",
+  },
+  {
+    "isMe": true,
+    "text": "Yup!!!",
+    "time": "11:19 am",
+  },
+  {
+    "isMe": false,
+    "text": "Enjoy!!",
+    "time": "11:20 am",
+  },
+];
+
+var statusList = {
+  0: ['Temi', 'assets/images/16.jpg', 3.0, 4.5, '1 minute ago', false],
+  1: ['Farouq Brother', 'assets/images/15.jpg', 1.0, 0.0, '20 minutes ago', false],
+  2: [
+    'Asmaa Sister',
+    'assets/images/17.jpg',
+    5.0,
+    2.5,
+    '28 minutes ago',
+    false,
+  ],
+  3: ['Soad Sister', 'assets/images/18.jpg', 2.0, 3.5, '53 minute ago', false],
+  4: ['Heba Sister', 'assets/images/14.jpg', 4.0, 3.0, '40 minute ago', false],
+};
